ffd50073e52fb16dc277df43cfcfd8e4798ca31f
ajaici <ajaici@gmail.com>
2020-09-02 14:23:48 +0530

    specification,automapper, configuring automapper, enabling static content

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,45 +19,87 @@ namespace API.Controllers
         private readonly IGenericRepository<ProductType> _productType;
         private readonly IGenericRepository<ProductBrand> _productBrand;
         private readonly IGenericRepository<Product> _product;
-        public ProductsController(IGenericRepository<Product> product, IGenericRepository<ProductBrand> productBrand, IGenericRepository<ProductType> productType)
+
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> product, IGenericRepository<ProductBrand> productBrand,
+         IGenericRepository<ProductType> productType,
+         IProductRepository productRepository, IMapper mapper
+         )
         {
+            _mapper = mapper;
+            _productRepository = productRepository;
             _product = product;
             _productBrand = productBrand;
             _productType = productType;
         }
 
         [HttpGet]
-        public async Task<IReadOnlyList<Product>> GetProducts()
+        public async Task<IReadOnlyList<ProductToReturnDto>> GetProducts()
         {
             //return "List of products";
 
-            //return await _repo.GetProductsAsync();
+            // return await _productRepository.GetProductsAsync();
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+
+            //return await _product.ListAsync(spec);
+            var products = await _product.ListAsync(spec);
+
+            return _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            // return products.Select(product => new ProductToReturnDto
+            // {
+            //     Id = product.Id,
+            //     Name = product.Name,
+            //     PictureUrl = product.PictureUrl,
+            //     Description = product.Description,
+            //     ProductBrand = product.ProductBrand.Name,
+            //     ProductType = product.ProductType.Name
+            // }).ToList();
 
 
-            return await _product.GetAllAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<Product> GetProduct(int id)
+        public async Task<ProductToReturnDto> GetProduct(int id)
         {
-            return await _product.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _product.GetEntityWithSpec(spec);
+
+            // return new ProductToReturnDto
+            // {
+            //     Id = product.Id,
+            //     Name = product.Name,
+            //     Description = product.Description,
+            //     PictureUrl = product.PictureUrl,
+            //     ProductBrand = product.ProductBrand.Name,
+            //     ProductType = product.ProductType.Name
+
+            // };
+
+            return _mapper.Map<Product, ProductToReturnDto>(product);
+
+            // return await _product.GetEntityWithSpec(spec);
+
+            //return await _productRepository.GetProductByIdAsync(id);
 
         }
 
-         [HttpGet("Brands")]
-         public async Task<IReadOnlyList<ProductBrand>> GetProductBrands()
-         {
-             //return "List of products";
-
-             return await _productBrand.GetAllAsync();
-         }
-
-         [HttpGet("Types")]
-        public async Task<IReadOnlyList<ProductType>> GetProductTypes()
-         {
+        [HttpGet("Brands")]
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrands()
+        {
             //return "List of products";
 
-           return await _productType.GetAllAsync();
-         }
+            return await _productBrand.GetAllAsync();
+        }
+
+        [HttpGet("Types")]
+        public async Task<IReadOnlyList<ProductType>> GetProductTypes()
+        {
+            //return "List of products";
+
+            return await _productType.GetAllAsync();
+        }
     }
 }
---
+++ API/Helpers/ProductUriResolver.cs
@@ -0,0 +1,28 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUriResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+        public ProductUriResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+               return _config["APIUrl"] + source.PictureUrl;
+            }
+
+            return null;
+
+        }
+    }
+}
